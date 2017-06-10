d2d537922a4dd5cde8d63264cc3d990acba86e5a
Simon Nonnis <simononnis@gmail.com>
2017-06-10 16:59:30 +0100

    'Clean up'

--- src/App.js
+++ src/App.js
@@ -1,21 +1,29 @@
 // @flow
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Form />
     );
+  }
+}
+
+class Form extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      data: {}
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Hello world</h1>
+      </div>
+    )
   }
 }
 
