b2a8d835566cc5da1d9549c7bb947687c823202d
Simon Nonnis <simononnis@gmail.com>
2017-06-10 17:46:44 +0100

    'Add form component'

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 // @flow
 import React, { Component } from 'react';
 
-class App extends Component {
+const options1 = ['orange', 'banana', 'mango', 'pear'];
+const options2 = ['dog', 'tiger', 'cat'];
+const name1 = 'fruits';
+const name2 = 'animals';
+
+
+
+// App component
+export default class App extends Component {
   render() {
     return (
       <Form />
@@ -9,6 +17,8 @@ class App extends Component {
   }
 }
 
+
+// Form component
 class Form extends Component {
   constructor(props) {
     super(props)
@@ -16,15 +26,46 @@ class Form extends Component {
     this.state = {
       data: {}
     }
+
+    this.onFormSubmit = this.onFormSubmit.bind(this);
+  }
+
+  onFormSubmit(event) {
+    event.preventDefault();
+    console.log('Form submitted!');
   }
 
   render() {
     return (
-      <div>
-        <h1>Hello world</h1>
-      </div>
+      <form onSubmit={this.onFormSubmit}>
+        <h1>React Long Form</h1>
+
+        <RadioGroup optionsAvailables={options1} name={name1} />
+        <RadioGroup optionsAvailables={options2} name={name2} />
+
+        <button>Save</button>
+      </form>
     )
   }
 }
 
-export default App;
+
+// RadioGroup component
+class RadioGroup extends Component {
+  render() {
+    const { optionsAvailables } = this.props;
+    const { name } = this.props;
+    const optionsGroup = optionsAvailables.map((option, i) => {
+      return (
+        <div key={i} >
+          <input type="radio" name={name}/>
+          <label>{option}</label>
+        </div>
+      )
+
+    })
+    return (
+      <div>{optionsGroup}</div>
+    )
+  }
+}
